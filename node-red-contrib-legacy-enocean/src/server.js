ea7bc37f3c2b00dc8a324e586159fa85e850a2dd
Janne Hietamäki <->
2020-03-20 17:49:36 +0200

    cleanup for stuck connections

--- node-red-contrib-legacy-enocean/src/server.js
+++ node-red-contrib-legacy-enocean/src/server.js
@@ -9,6 +9,13 @@ const port = new SerialPort('/dev/ttyUSB0', { baudRate: 57600 })
 const parser = new ESP3Parser()
 port.pipe(parser)
 
+
+const noop = () => {};
+
+function heartbeat() {
+  this.isAlive = true;
+}
+
 const wss = new WebSocket.Server({ port: 8083 });
 
 let connection;
@@ -21,11 +28,22 @@ const handleAction = (ws) => (message) => {
 wss.on('connection', (ws) => {
   connection = ws;
   console.log('WebSocket connected', ws);
+  ws.isAlive = true;
+  ws.on('pong', heartbeat);
   ws.on('message', handleAction(ws));
 });
 
+const interval = setInterval(() => {
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) return ws.terminate();
+    ws.isAlive = false;
+    ws.ping(noop);
+  });
+}, 30000);
+
 wss.on('close', () => {
   console.log('WebSocket connection closed');
+  clearInterval(interval);
 });
 
 const send = (data) => {
